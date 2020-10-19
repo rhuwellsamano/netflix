3c925dda31afc904fc0fdd7079794b463ad5f03d
rhuwellsamano <rhuwell.samano@gmail.com>
2020-10-19 13:52:29 -0400

    project init - working on jumbotron

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Inner } from './styles/jumbotron';
+
+function Jumbotron({}){
+    return(
+        <Inner>
+            <p>Hello again Rhu!</p>
+        </Inner>
+    )
+}
+
+export default Jumbotron;
